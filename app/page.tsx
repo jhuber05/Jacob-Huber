ba49ba3c4210209b2f09c930b24bfe8b551cc452
Louis Rolwes <lbRolwes@gmail.com>
2024-10-08 23:13:55 -0500

    oct7 creating homepage to get Jacob started

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center bg-blue-500 ">
+      <h1 className="text-3xl font-bold text-white">Jacob Huber's Website</h1>
+      <button onClick={()=>{alert("hello");}} className="bg-white text-blue-500 px-4 py-2 rounded-lg mt-4 hover:-translate-y-2 transistion duration-500 ease-in-out">
+        Say Hi
+      </button>
+    </div>
+  );
+}
