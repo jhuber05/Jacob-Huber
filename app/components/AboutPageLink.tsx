e0affe253ec8fc6db1ec1accfd69a60374d82f28
jhuber05 <jdhhuber@gmail.com>
2024-10-16 12:06:58 -0500

    Made the buttons on the panel the same color as the background

--- app/components/AboutPageLink.tsx
+++ app/components/AboutPageLink.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function AboutLink() {
   return (
-    <Link href="/about" className="bg-white text-blue-500 px-4 py-2 rounded-sm hover:-translate-y-2 transition duration-500 ease-in-out">
+    <Link href="/about" className="bg-blue text-white-500 px-4 py-2 rounded-sm hover:-translate-y-2 transition duration-500 ease-in-out">
         About Page
     </Link>
       
