bb3bc1640cbfb711ca60f5781a5812d4f2331bf8
jhuber05 <jdhhuber@gmail.com>
2024-10-20 19:25:43 -0500

    Added nav bar to the about page but need to learn how to make it a component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,22 @@
 "use client";
+import Image from "next/image";
+import FaqsLink from "@/app/components/FaqsPageLink";
+import LearnMoreLink from "@/app/components/LearnMorePageLink";
 
 
 export default function About() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center bg-black ">
+    <div className="w-screen min-h-screen flex flex-col items-center justify-center bg-custom-blue ">
+      <div className="w-full flex flex-row justify-end items-center p-4 bg-blue-800 space-x-4">
+        <FaqsLink />
+        <LearnMoreLink />
+      </div>
       <h1 className="text-3xl font-bold text-white">Jacob Huber&apos;s About Page</h1>
       <button onClick={()=>{alert("hello");}} className="bg-white text-blue-500 px-4 py-2 rounded-sm mt-4 hover:-translate-y-2 transistion duration-500 ease-in-out">
         Say Hi
       </button>
+      <Image src="/deltapsi.jpeg" width={500} height={500} className="h-[50px] w-[100px] opacity-50" alt="Image of Delta Psi"/> 
+      <div className="w-full h-[500px] bg-white md:bg-blue-400 lg:bg-black"/>  
     </div>
   );
 }
